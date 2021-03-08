923c8f7920eef0790f0993029df92f3bcbbc1404
Ben LeValley <ben.levalley@gmail.com>
2021-03-08 10:30:39 -0600

    Creating objects to hold the fully resolved DAG

--- git-align/Gitalign/Types.hs
+++ git-align/Gitalign/Types.hs
@@ -1,4 +1,31 @@
-module Gitalign.Types where
+module Gitalign.Types 
 
-foo :: Int
-foo = 1
+where
+
+import Data.Text (Text)
+import qualified Data.HashMap.Strict as HM
+
+newtype ObjectRepository = ObjectRepositoryT { unObjectRepository ::  HM.HashMap SHA Commit }
+
+newtype SHA = SHA Text
+type ObjectName = Text
+type Email = Text
+type TimestampUTC = Integer
+type OffsetUTC = Integer
+
+
+data Blob = BlobT SHA Text
+data Tree = TreeT SHA ObjectName [Blob] [Tree]
+data Commit = CommitT {
+    commitSha :: SHA,
+    commitTree :: Tree,
+    commitParents :: [Commit],
+    commitMessage :: Text,
+    commitAuthor :: Contributor,
+    commitCommitter :: Contributor
+}
+
+
+data Contributor = ContributerT ObjectName Email TimestampUTC OffsetUTC
+
+
