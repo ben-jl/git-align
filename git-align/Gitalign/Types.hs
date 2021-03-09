89d095919f6d9408cf4c52fe6d3b2d03288a89f4
Ben LeValley <ben.levalley@gmail.com>
2021-03-09 09:05:13 -0600

    Refactoring core git types, trying to get the signature for DAG right

--- git-align/Gitalign/Types.hs
+++ git-align/Gitalign/Types.hs
@@ -1,87 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Gitalign.Types 
 
 where
 
-import Data.Text (Text, unpack)
-import Data.Text.Encoding (encodeUtf8)
-import qualified Data.HashMap.Strict as HM
-import Data.Hashable as H
-import qualified Data.ByteString as B
-import Data.String (IsString(..))
+import qualified Data.Graph.Types as G
+import qualified Data.Graph.DGraph as D
+import qualified Data.Hashable as H
+import Data.Text ( Text ) 
+import Data.String (IsString(fromString))
+                
+data Commit = CommitT { commitSHA :: Text, commitParents :: [Commit] } deriving Show
+instance H.Hashable Commit where
+    hashWithSalt s = H.hashWithSalt s . commitSHA
 
-newtype SHA = SHA B.ByteString deriving (Show, Eq)
-instance IsString SHA where
-    fromString = SHA . fromString
+instance Eq Commit where
+    (==) c1 c2 = commitSHA c1 == commitSHA c2
 
-data EnrichedBy a = SHAOnly SHA | FullValue a deriving (Show, Eq)
+instance IsString Commit where
+    fromString s = CommitT (fromString s) [] 
 
-type ObjectName = Text
-type Email = Text
-type TimestampUTC = Integer
-type OffsetUTC = Integer
+newtype Repository = RepositoryT { unRepo :: D.DGraph Commit () } deriving Show
 
-data Blob = BlobT SHA (EnrichedBy Text) deriving (Show, Eq)
-instance RetrievableBySHA Blob where sha (BlobT x _) = x
-instance Hashable Blob where hashWithSalt s (BlobT sh _) = hashWithSalt s sh
+parents :: Commit -> Repository -> [Commit]
+parents c r = G.reachableAdjacentVertices (unRepo r) c
 
-data Tree = TreeT SHA ObjectName [EnrichedBy Blob] [EnrichedBy Tree] deriving (Show, Eq)
-instance RetrievableBySHA Tree where sha (TreeT x _ _ _) = x
-instance Hashable Tree where hashWithSalt s (TreeT sh _ _ _) = hashWithSalt s sh
-
-data Commit = CommitT {
-    commitSha :: SHA,
-    commitTree :: EnrichedBy Tree,
-    commitParents :: [EnrichedBy Commit],
-    commitMessage :: Text,
-    commitAuthor :: Contributor,
-    commitCommitter :: Contributor
-} deriving (Show, Eq)
-instance RetrievableBySHA Commit where sha = commitSha
-instance Hashable Commit where hashWithSalt s = hashWithSalt s . commitSha
-
-data Contributor = ContributerT ObjectName Email TimestampUTC OffsetUTC deriving (Show, Eq)
-
-class RepositoryContext r where
-    repoContents :: RetrievableBySHA b => r [b]
-    lookupBySHA :: RetrievableBySHA b => r [b] -> SHA -> b
-
-class RetrievableBySHA a where
-    sha :: a -> SHA
-
-instance Hashable SHA where
-    hashWithSalt s (SHA x) = hashWithSalt s x
-
-unwrap :: SHA -> B.ByteString
-unwrap (SHA x) = x
-
-data GitObject = CommitObj Commit | TreeObj Tree | BlobObj Blob
-instance (RetrievableBySHA GitObject) where 
-    sha o = case o of
-                CommitObj comm -> sha comm
-                TreeObj tree -> sha tree
-                BlobObj blob -> sha blob
-
-data DAG = Head | Node Commit [DAG] deriving (Show, Eq)
-
-                            
-newtype Repository = RepositoryT { traceBack :: Int -> Commit -> DAG }
-fromObjectRepo :: ObjectRepository GitObject -> Repository
-fromObjectRepo r = let resolveRepo hm acc n c = 
-                        case n of 
-                            0 -> acc
-                            n -> resolveRepo hm (Node c [acc]) (n-1) $ CommitT "foo" undefined undefined undefined undefined undefined  in
-                   RepositoryT $ resolveRepo (unRepo r) Head
-
-newtype ObjectRepository a = ObjectRepositoryT { unRepo :: RetrievableBySHA a => HM.HashMap SHA [a] }
-
-objectRepoFromList :: (RetrievableBySHA a) => [a] -> ObjectRepository a
-objectRepoFromList l = 
-    let 
-        objectRepoFromList' [] acc = acc
-        objectRepoFromList' (x:xs) acc 
-            = let updatedMap = 
-                    case HM.lookup (sha x) acc of
-                        Just curr -> HM.adjust (\_ -> x:curr) (sha x) acc
-                        Nothing -> HM.insert (sha x) [x] acc 
-               in objectRepoFromList' xs updatedMap
-    in ObjectRepositoryT $ objectRepoFromList' l HM.empty
+fromCommitList :: [Commit] -> Repository
+fromCommitList cts = 
+    let fromCommitList' [] acc = RepositoryT acc
+        fromCommitList' (c:cs) acc = fromCommitList' cs (G.insertEdgePairs ((c,) <$> commitParents c) acc)
+    in fromCommitList' cts G.empty
--- git-align/test/Test/ConstructorSpec.hs
+++ git-align/test/Test/ConstructorSpec.hs
@@ -2,28 +2,22 @@
 module Test.ConstructorSpec (constructorSpec) where
 import Test.HUnit as H
 import Data.Text
-import Data.Function ((&))
-import Gitalign 
-import qualified Data.HashMap.Strict as HM
-import Data.Functor
-import Data.Maybe
+import Gitalign
 
 
 constructorSpec :: IO ()
-constructorSpec = 
-    let testList = H.TestList [
-            "Creates graph from list" H.~: createsGraphFromList
-            ] :: H.Test
-        in
-    H.runTestTTAndExit testList
-        
-    
-createsGraphFromList :: H.Test
-createsGraphFromList = let input = [BlobT (SHA "foobar") (FullValue "cabaret"), BlobT (SHA "foobar") (FullValue "bar"), BlobT (SHA "cape") (FullValue "car")] 
-                           hashmap = unRepo (objectRepoFromList input) in 
-    TestList [
-         maybe 0 Prelude.length (HM.lookup (SHA "foobar") hashmap) H.~=? 2,
-         HM.lookup (SHA "cape") hashmap H.~=? Just [BlobT (SHA "cape") (FullValue "car")]
+constructorSpec = H.runTestTTAndExit
+    simpleObjGraphConstruction
 
-    ] 
-
+simpleObjGraphConstruction :: H.Test 
+simpleObjGraphConstruction = 
+                                -- foo -- bar
+                                --     \
+                                --      ten -- excavate
+    let repo = fromCommitList [CommitT "foo" ["bar", "ten"], CommitT "bar" [], CommitT "ten" ["excavate"], CommitT "excavate" []] 
+        numParents c = Prelude.length (parents c repo) in
+    H.TestList [
+            "it (foo) has the correct number of parents" H.~: numParents (CommitT "foo" [])  H.~?= 2, 
+            "it (ten) has the correct number of parents" H.~: numParents (CommitT "ten" []) H.~?= 1,
+            "it (bar) has the correct number of parents" H.~: numParents (CommitT "bar" []) H.~?= 0,
+            "it (excavate) has the correct number of parents" H.~: numParents (CommitT "excavate" []) H.~?= 0 ]
